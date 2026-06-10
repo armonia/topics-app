b61d5a4e4fb7a0cbac4a48a99608911bc9740f6b
Attilio Cianci <zorahrel@gmail.com>
2026-06-10 22:03:15 +0200

    chore(landing): rebuild demo bundle with the header/shortcut polish

--- landing/app/assets/ActivityPane-BW_9C-JB.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./markdown-BSyWqnvk.js";import{ActivityFeedPanel as r}from"./ActivityFeedPanel-DNNw3hg8.js";import"./icons-qYTwN--u.js";import"./index-CqQJTOUs.js";import"./react-vendor-D25dlSlT.js";function p(){return t.jsx(r,{enabled:!0})}export{p as ActivityPane};
