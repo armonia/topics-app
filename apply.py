bd17abec9eeb0a4513d1caf79234e8a24ef447b5
Attilio Cianci <zorahrel@gmail.com>
2026-09-14 14:44:19 +0200

    Via i file di servizio finiti nel commit precedente

--- apply.py
+++
@@ -1,5 +0,0 @@
-p = 'client/src/components/Browser/TopicBrowserWindow.tsx'
-L = open(p).read().split('\n')
-L[53] = '  resolveComposerAvoidance,'
-assert 'ComposerFloor' in L[360]
-L[360] = open('newline.txt').read().rst… [46 chars dropped to fit the context window]
